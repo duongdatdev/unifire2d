bef183677e4d3049e948948185a90c4af854b844
DuongDat <duongdat030@gmail.com>
2025-11-01 03:45:10 +0700

    upload basic version of game - Player can fire - Spawn asteroid - Basic collider between asteroid and bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 12f;
+    public float lifeTime = 2f;
+    
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+    
+    void Update()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Asteroid"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
